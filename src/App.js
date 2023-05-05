3b8c00a373b7372464f0ad16f6a8a7f04569a810
Erik Varga <viglew@gmail.com>
2023-05-05 15:01:11 -0400

    created jsx pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+import Offers from "./pages/Offers";
+
 
 function App() {
   return (
     <>
-      <h1 className='p-2 text-2xl bg-red-400'>Hello Asshole!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+      
     </>
   );
 }
