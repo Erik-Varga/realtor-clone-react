9a713daed4570d165c82af15b04e718dd345a874
Erik Varga <viglew@gmail.com>
2023-05-06 09:04:20 -0400

    created private route, custom profile hook and log-out functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,20 @@ import { AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <div className='card-footer m-2 p-2 hover:text-blue-600 hover:font-semibold transition duration-400 ease-in-out'>
-        <Link to="https://github.com/Erik-Varga" target="_blank" className='flex items-center justify-center'>
-            &copy; {(new Date().getFullYear())} Erik Varga | Web Developer 
-            
-            <AiFillGithub className='ml-1' />
+    <div className='flex justify-center items-center card-footer m-1 p-1 text-sm hover:text-blue-600 hover:font-semibold transition duration-400 ease-in-out'>
+        <span>
+          <Link to="https://github.com/Erik-Varga" target="_blank" className='flex items-center justify-center'>
+              &copy; {(new Date().getFullYear())} Erik Varga | Web Developer 
+              
+              <AiFillGithub className='ml-1' />
+          </Link>
+        </span>
+        <span>
+        <Link to="https://github.com/Erik-Varga/realtor-clone-react" target="_blank" className='flex items-center justify-center'>
+          &nbsp;
+          Source Code
         </Link>
+        </span>
     </div>
-
-
-    // <div>
-    //   <a href="https://github.com/Erik-Varga" target="_blank">&copy; <script>document.write(new Date().getFullYear())</script> Erik Varga | Web Developer <i class="fa-brands fa-github"></i></a>
-    // </div>
-
-
-
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 export default function Profile() {
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  const [formData, setFormDate ] = useState({
+    fullname: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { fullname, email } = formData;
+
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+
   return (
-    <div>
-      Profile
-    </div>
+    <section className='max-width-6xl mx-auto flex flex-col justify-center items-center'>
+      <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
+      <div className='w-full md:w-[50%] mt-6 px-3'>
+        <form>
+          {/* Name Input */}
+          <input type='text' id='fullname' value={fullname} disabled className='w-full mb-4 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-300 ease-in-out'></input>
+          
+          {/* Email Input */}
+          <input type='email' id='email' value={email} disabled className='w-full mb-4 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-300 ease-in-out'></input>
+
+          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Do you want to change your profile?
+                <Link
+                  to="/profile"
+                  className="text-red-600 hover:text-blue-700 hover:font-semibold transition duration-800 ease-in-out ml-1"
+                >
+                  Edit
+                </Link>
+              </p>
+              <p className="text-blue-600 hover:text-red-800 hover:font-semibold transition duration-800 ease-in-out cursor-pointer" onClick={onLogout}
+                >
+                  Sign Out
+              </p>
+            </div>
+
+            {/* Login */}
+            {/* <button
+              className="w-full px-7 py-3 bg-blue-600 text-white text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg transition duration-150 ease-in-out active:bg-blue-800"
+              type="submit"
+            >
+              Register
+            </button> */}
+
+
+        </form>
+      </div>
+    </section>
   )
 }
