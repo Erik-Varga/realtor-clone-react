815d5eff16bf951415a0d2af3b26bf69fba2e71a
Erik Varga <viglew@gmail.com>
2023-05-17 15:15:30 -0400

    updated listingItem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -139,8 +139,8 @@ export default function Profile() {
       <div>
         {!loading && listings.length > 0 && (
           <div className='max-w-6xl px-3 mt-6 mx-auto'>
-            <h2 className="text-2xl text-center font-semi">My Listings</h2>
-            <ul>
+            <h2 className="text-2xl text-center font-semibold mb-6">My Listings</h2>
+            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6'>
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
