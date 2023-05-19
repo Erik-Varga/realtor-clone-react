b503c20245d9087bd3672003b1e9b1825b463aba
Erik Varga <viglew@gmail.com>
2023-05-19 15:10:26 -0400

    added share link functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,6 +8,7 @@ import Spinner from '../components/Spinner';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from 'swiper';
 import "swiper/css/bundle";
+import { FaShare } from 'react-icons/fa';
 
 export default function Listing() {
     const navigate = useNavigate();
@@ -16,6 +17,7 @@ export default function Listing() {
     // const [geolocationEnabled, setGeoLocationEnabled] = useState(false);
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false);
     SwiperCore.use([Autoplay, Navigation, Pagination]);
     
     const params = useParams();
@@ -62,6 +64,18 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className='fixed top-[7%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center' onClick={()=>{
+        navigator.clipboard.writeText(window.location.href);
+        setShareLinkCopied(true);
+        setTimeout(()=>{
+          setShareLinkCopied(false);
+        }, 3000);
+      }}>
+        <FaShare className='text-lg text-slate-500'/>
+      </div>
+      {shareLinkCopied && (
+        <p className='fixed top-[7%] right-[15%] z-20 font-semibold border-2 border-gray-200 round-md bg-white text-green-800 p-2'>Link Copied!</p>
+      )}
     </main>
   );
 }
